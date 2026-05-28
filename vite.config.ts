a5d96cad6c3dc0aa53b6f9feb63eecfba56f38cd
mgdakcz <magda.kuczke@gmail.com>
2026-05-28 21:32:45 +0200

    update script to pass Nitro

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,6 @@ export default defineConfig({
     // nitro/vite builds from this
     server: { entry: "server" },
   },
+  // Force enable Nitro even when outside of the Lovable development environment
+  nitro: true,
 });
